b8b927a38271ecb7538ac7780489f074d865f3df
VSMent <vsmentvs@gmail.com>
2021-01-07 22:55:21 +0200

    train, save net to JSON, trained jsons

--- train.js
+++ train.js
@@ -1,14 +1,18 @@
 const brainjs = require("brainjs");
-const {loadDataset,drawToCli} = require("./js/utils.js");
+const {loadDataset, drawToCli, saveNet} = require("./js/utils.js");
 const datasets = {
     loaded: {},
     names: ['balanced', 'byclass', 'bymerge', 'digits', 'letters', 'mnist']
 }
 
 const net = new brainjs.NeuralNetwork();
-datasets.loaded.mnist = loadDataset();
-drawToCli(datasets.loaded.mnist[5].input);
-console.log(datasets.loaded.mnist[5].output);
+datasets.loaded.mnistTr = loadDataset();
+let trainData = [...datasets.loaded.mnistTr.slice(0, 100)];
+net.train(trainData, {
+    log: true
+});
+
+saveNet(net.toJSON(),'mnist100');
 // console.log(datasets.loaded.mnist[134]);
 
 
