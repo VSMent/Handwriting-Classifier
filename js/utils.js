caf91680a24d21f558b9aa87b8608b1cf083b0d9
VSMent <vsmentvs@gmail.com>
2021-01-08 01:43:26 +0200

    draw image wider, get label for balanced dataset

--- js/utils.js
+++ js/utils.js
@@ -1,13 +1,20 @@
 const fs = require('fs');
 
 function loadDataset(type = 'train', name = 'mnist') {
+    const getLabel = (labelIdx) => {
+        if (name === 'balanced') {
+            return "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabdefghnqrt"[labelIdx];
+        } else {
+            return labelIdx;
+        }
+    };
     const imageBytes = 784;
     const ds = [];
     const labels = fs.readFileSync(`./EMNIST/${name}/emnist-${name}-${type}-labels-idx1-ubyte`).slice(8);
     const images = fs.readFileSync(`./EMNIST/${name}/emnist-${name}-${type}-images-idx3-ubyte`).slice(16);
     for (let i = 0; i < labels.length; i++) {
         ds.push({
-            output: {[labels[i]]: 1},
+            output: {[getLabel(labels[i])]: 1},
             input: rotate(scaleDown(images.slice(i * imageBytes, (i + 1) * imageBytes)))
         });
     }
@@ -24,18 +31,18 @@ function scaleDown(arr) {
 
 function drawToCli(image) {
     const shades = ' .:-=+*#%@';
-    console.log(`+${'-'.repeat(28)}+`);
+    console.log(`+ ${'- '.repeat(28)}+`);
     for (let i = 0; i < 28; i++) {
         let line = '';
         for (let j = 0; j < 28; j++) {
             let value = image[i * 28 + j];
             value = value !== 0 ? value - 0.0001 : value;
             value = Math.floor(value * 10);
-            line += shades[value];
+            line += shades[value] + ' ';
         }
-        console.log(`|${line}|`);
+        console.log(`| ${line}|`);
     }
-    console.log(`+${'-'.repeat(28)}+`);
+    console.log(`+ ${'- '.repeat(28)}+`);
 }
 
 function rotate(arr) {
