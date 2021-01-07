8ef991872ee6241a4f884bc40bd70ac64a0468a9
VSMent <vsmentvs@gmail.com>
2021-01-08 01:46:45 +0200

    load balanced dataset

--- train.js
+++ train.js
@@ -6,26 +6,34 @@ const datasets = {
 }
 
 const net = new brainjs.NeuralNetwork();
+const dateBefore = new Date();
 
-// datasets.loaded.mnistTr = loadDataset();
+datasets.loaded.balancedTr = loadDataset('train', 'balanced');
 // let trainData = [...datasets.loaded.mnistTr.slice(0, 100)];
-// net.train(trainData, {
-//     log: true,
-//     errorThresh: 0.005
-// });
-// saveNet(net.toJSON(), 'mnist100');
+let trainData = [...datasets.loaded.balancedTr];
+net.train(trainData, {
+    log: true,
+    // errorThresh: 0.003,
+});
 
-net.fromJSON(loadNet('mnist'))
+const dateAfter = new Date();
+const seconds = Math.floor((dateAfter - (dateBefore)) / 1000);
+const minutes = Math.floor(seconds / 60);
+const hours = Math.floor(minutes / 60);
+console.log(`It took ${hours}:${minutes}:${seconds} to train net`)
+saveNet(net.toJSON(), 'balanced');
 
-datasets.loaded.mnistTe = loadDataset('test');
-let testData = [...datasets.loaded.mnistTe];
+net.fromJSON(loadNet('balanced'))
+
+datasets.loaded.balancedTe = loadDataset('test', 'balanced');
+let testData = [...datasets.loaded.balancedTe];
 let error = 0;
 for (let i = 0; i < testData.length; i++) {
     let o = Object.keys(testData[i].output)[0];
     let r = topNPredictions(net.run(testData[i].input), 1)[0][0];
     if (o !== r) {
-        console.log(o, topNPredictions(net.run(testData[i].input), 5));
-        drawToCli(testData[i].input);
+        // console.log(o, topNPredictions(net.run(testData[i].input), 5));
+        // drawToCli(testData[i].input);
         error++;
     }
 }
